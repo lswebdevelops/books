d79dfe9b997c6007ab30fbbb435405a283eb1836
lswebdevelops <sardanhaluciano9690@gmail.com>
2025-03-14 19:21:11 -0300

    ready for deploy

--- backend/server.js
+++ backend/server.js
@@ -47,18 +47,18 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 // uncomment for production build
 // then add this script to the root package.json (   "build": "npm install && npm install --prefix frontend && npm run build --prefix frontend") and to .env :> NODE_ENV=development change to production
 
-// if (process.env.NODE_ENV === 'production') {
-//   //  set static folder
-//   app.use(express.static(path.join(__dirname, '/frontend/build')));
+if (process.env.NODE_ENV === 'production') {
+  //  set static folder
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
 
-//   // any route that is not api will be redirected to index.html
-//   app.get('*', (req, res) => 
-//   res.sendFile(path.resolve(__dirname, 'frontend', 'build' , 'index.html')))
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("API is running...");
-//   });
-// }
+  // any route that is not api will be redirected to index.html
+  app.get('*', (req, res) => 
+  res.sendFile(path.resolve(__dirname, 'frontend', 'build' , 'index.html')))
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
 // comment until here
 
 app.use(notFound);
