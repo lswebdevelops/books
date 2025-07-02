54ff2d861116c3558450f18aa06275352c9d0048
lswebdevelops <sardanhaluciano9690@gmail.com>
2025-07-02 15:35:03 -0300

    changing constants (blog upload variable)

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -5,6 +5,6 @@ export const BOOKS_URL = "/api/books";
 export const USERS_URL = "/api/users";
 export const ORDERS_URL = "/api/orders";
 export const PAYPAL_URL = "/api/config/paypal";
-export const UPLOAD_URL = "/api/blogs/upload";
+export const UPLOAD_BLOG_URL = "/api/blogs/upload";
 export const POEMS_URL = "/api/poems"; 
 export const BLOGS_URL = "/api/blogs"; 
