f90db03523500bd6e3952bf892cebf5183b1bb2f
lswebdevelops <sardanhaluciano9690@gmail.com>
2025-07-02 20:09:19 -0300

    added some changes

--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { protect, admin } from "../middleware/authMiddleware.js";
-import { upload } from "../middleware/uploadMiddleware.js";
+import { upload } from "../middleware/uploadBlogImagesMiddleware.js";
 import checkObjectId from "../middleware/checkObjectId.js";
 
 import {
